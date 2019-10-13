94e8a1f0fb1587d44d10e4b8f888718b3061a6f9
Devangib <bhuvadevangi9@gmail.com>
2019-10-13 13:26:38 +0530

    Update login_validate_user.php

--- login_validate_user.php
+++ login_validate_user.php
@@ -7,21 +7,23 @@
 	$uname=$_POST['username'];
 	$password=$_POST['password'];
 	$password1=md5($password);
+    $flag=0;
 	$sql = "SELECT * FROM `PublicTable` WHERE `public_email` = '$uname' AND `password` = '$password1'";
 	$result = mysqli_query($con, $sql);
 	while($row = mysqli_fetch_assoc($result)) {
-		if($row["email"] == $uname && $row["password"] == $password1){
+		if($row["public_email"] == $uname && $row["password"] == $password1){
 			$flag = 1;
 			break;
 		}
 	}
 	if($flag == 1)
 	{
-		header("Location: http://localhost/online_course/after_login.php");
+		header("Location: after_login.php");
 	}
 	else{
 		$_SESSION['loginfail']=1;
-		header("Location: index.php");
+        echo "error";
+		//header("Location: index.php");
 		exit();
 	}	
 ?>

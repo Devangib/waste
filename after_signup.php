4fadf3115fb80ba316240db2d09963bc7fa90e90
Devangib <bhuvadevangi9@gmail.com>
2019-10-13 13:25:42 +0530

    Update after_signup.php

--- after_signup.php
+++ after_signup.php
@@ -6,11 +6,11 @@
 	$uname=$_POST['username1'];
 	//$password=$_POST['password1'];
 	$password_hash=md5($_POST['password1']);
-	$sql = "INSERT INTO `PublicTable` (`public_email`, `password`, `street1`, `street2`, `area`, `city`, `rewards`) VALUES ('$user_email', $psw1, '$street1', '$street2', '$area', '$city', 0) ";
+	$sql = "INSERT INTO `PublicTable` (`public_email`, `password`) VALUES ('$uname', '$password_hash') ";
 	$result = mysqli_query($con, $sql);
 	$errorMessage = 0;
 	while($row = mysqli_fetch_assoc($result)) {
-		if($row["email"] == $uname){
+		if($row["public_email"] == $uname){
 			$errorMessage = 1;
 			break;
 		}
@@ -18,12 +18,13 @@
 	if($errorMessage == 1)
 	{
 		$_SESSION['errorMessage'] = 1;
-		header("Location: index.php");
+		//header("Location: index.php");
+        echo "error";
 	}
 	else{
 			$_SESSION['email']=$uname;
 			$_SESSION['password']=$password_hash;
-			header("Location: index.php");
+			//header("Location: index.php");
 		exit();
 	}	
 ?>
